34dc977592c7576bb70f50a64adc5fc721b19c47
Jesski <jess@jesski.com>
2024-10-01 13:04:23 -0500

    add types

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Health check endpoint
-app.get('/health', (req, res) => {
-    console.log('Health check is okay!');
-    res.status(200).send('OK');
+app.get('/health', (req: Request, res: Response) => {
+  console.log('Health check is okay!');
+  res.status(200).send('OK');
 });
 
 app.listen(port, () => {
